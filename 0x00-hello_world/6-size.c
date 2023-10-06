36fa2c2edf0343a4b090bd3176ff00a70f07b0b8
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-10-06 07:39:15 -0700

    Size printer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ long longType;
 
 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu bytes(s)\n", sizeof(intType));
-printf("Size of a long long: %zu byte(s)\n", sizeof(longlongType));
 printf("Size of a long: %zu byte(s)\n", sizeof(longType));
+printf("Size of a long long: %zu byte(s)\n", sizeof(longlongType));
 printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 
 return (0);
