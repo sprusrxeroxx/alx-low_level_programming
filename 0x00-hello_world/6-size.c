cce0d873d0365265afd0252b233ddb1b6cb06ee6
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-10-06 07:36:01 -0700

    Size printer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Prints "sizes of different data types
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int intType;
+float floatType;
+long long longlongType;
+char charType;
+long longType;
+
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu bytes(s)\n", sizeof(intType));
+printf("Size of a long long: %zu byte(s)\n", sizeof(longlongType));
+printf("Size of a long: %zu byte(s)\n", sizeof(longType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+
+return (0);
+}
+
