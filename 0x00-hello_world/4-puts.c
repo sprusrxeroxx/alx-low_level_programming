01f11535e34fef314a51ccadc3cda578b37ca0ed
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-10-06 04:54:28 -0700

    Betty checked

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
-#include <stdio.h>    
-int main(){
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+#include<stdio.h>
+int main(void);
+{	/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ *
+ * Return: Always 0 (Success)
+ */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
