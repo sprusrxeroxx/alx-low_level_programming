3ce00b94d8e183567078e10c0d11f5ac0beb3b04
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-10-12 19:25:27 -0700

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - matrix
+ * Return: no return
+ */
+
+void more_numbers(void)
+{
+
+int x, y;
+x = 1;
+y = 0;
+	while (x <= 10 )
+	{
+		for (y <= 14)
+		{
+			if (y >= 10)
+			_putchar('1');
+			_putchar(y % 10 + '0');
+			y++;
+		}
+			_putchar('\n');
+		}
+	x++;
+}
