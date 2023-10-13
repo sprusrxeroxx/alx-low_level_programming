47ceab8846917168201652fa4fada3ba73c3d4f4
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-10-12 19:03:13 -0700

    meh

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,16 +1,13 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * _mul - multiplies 2 intergers
+ * mul - multiplies 2 intergers
  * @a : first integer
  * @b : second integer
  * Return: always 0
  */
 
-int mul(int a, int b);
+int mul(int a, int b)
 {
-	_putchar("%d\n", mul(2, 8));
-	_putchar("%d\n", mul(222, 1));
-
-	return (0);
+	return (a * b);
 }
