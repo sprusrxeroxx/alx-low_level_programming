f8df92ceceac79f2ad203ca4605afc4702b845c2
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-10-12 18:55:59 -0700

    lost

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,15 +2,15 @@
 
 /**
  * _mul - multiplies 2 intergers
- * @x : first integer
- * @y : second integer
+ * @a : first integer
+ * @b : second integer
  * Return: always 0
  */
 
 int mul(int a, int b);
 {
-	_putchar("%d\n",mul(2, 8));
-	_putchar("%d\n",mul(222, 1));
+	_putchar("%d\n", mul(2, 8));
+	_putchar("%d\n", mul(222, 1));
 
 	return (0);
 }
