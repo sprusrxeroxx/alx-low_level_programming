fcc0b6b0a7a0d3bd864e7e1a0d289d4a2541491c
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-10-12 18:21:26 -0700

    lost

---
+++ 0x04-more_functions_nested_loops/isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main checks if c is a digit
+ * @c check character
+ * Return: 1 or 0
+ */
+
+
+
+int _isdigit(int c)
+{
+	if (c >= "0" && c <= '9')
+		return (1);
+	else
+		return (0);
+}
