34f9f4b3eee7483461b60690dd8ee6a9231fd7a0
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-10-12 19:32:36 -0700

    Print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - printing lines
+ * @n: number of times
+ *
+ * Return: void
+ */
+
+
+void print_line(int n)
+{
+	int x;
+
+	for (x = 0; i < n; x++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
