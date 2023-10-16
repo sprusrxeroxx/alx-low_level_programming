5d7966c60e515d3dac044afdb8bd83652606676a
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-10-16 13:50:46 -0700

    First commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- *main - takes pointer to an  "int" as a parameter
+ *reset_to_98 - takes pointer to an  "int" as a parameter
  *and re-assigns it to "98"
  *
  *@n : an int
