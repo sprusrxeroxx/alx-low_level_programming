d697b84086cc30e8bbe6e54b7a8814b4698da8a6
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-10-19 14:33:20 -0700

    Strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,17 +2,18 @@
 
 /**
  * _strcat - joins two strings
- * @str1: 1st string
- * @str2: 2nd string
+ * @dest: 1st string
+ * @src: 2nd string
  * Return: pointer to joined string
  */
 
-char *_strcat(char *dest, char *src) 
+char *_strcat(char *dest, char *src)
 {
-	int i = 0 , len = 0; 
-	while(dest[i++])
+	int i = 0, len = 0;
+
+	while (dest[i++])
 		len++;
-	for(i = 0; i < src[i]; i++)
+	for (i = 0; i < src[i]; i++)
 		dest[len++] = src[i];
 
 	return (dest);
