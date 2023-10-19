160b243811d6d51f5dbc69f3eefeabcbf8836061
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-10-19 15:07:45 -0700

    02-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,9 +15,12 @@ int i = 0, len = 0;
 
 while (src[i++])
 len++;
+
 for (i = 0; src[i] && i < n; i++)
-dest[len] = src[i];
+dest[i] = src[i];
+
 for (i = len; i < n ; i++)
 dest[i] = '\0';
+
 return (dest);
 }
