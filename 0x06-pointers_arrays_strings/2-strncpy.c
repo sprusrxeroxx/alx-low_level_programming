077430f11abef5062a956fe3c3e317e93961c96e
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-10-19 14:54:43 -0700

    2-Strcpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strncpy -joins two strings of parn
+ *@dest: string appended on
+ *@src: string appended to dest
+ *@n: length of bytes to join
+ * Return: a string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0, len = 0;
+
+while (dest[i++])
+len++;
+for (i = 0; src[i] && i < n; i++)
+dest[len++] = src[i];
+return (dest);
+}
