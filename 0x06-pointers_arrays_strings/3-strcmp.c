ea749329a8826dca02f3433b285a24ea5704b1d3
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-10-19 16:28:29 -0700

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strcmp - compares 2 strings
+ *@s1: string 1 
+ *@s2: string 2
+ *Return: s1 - s2 or 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+while (*s1 == *s2++)
+{
+if (*s1 == '\0')
+{
+return 0;
+}
+s1++;
+}
+return (*s1 - *s2);
+}
