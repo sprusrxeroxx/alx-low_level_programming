62335250a5ca57ed6ada6f84e7da99806c448dbf
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-10-19 18:19:51 -0700

    7-1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *encode_to_1337 - encodes a string into 1337.
+ *string: a string 
+ *Return: a string 
+ */
+
+char *encode_to_1337(char *string)
+{
+while (*string != '\0')
+{
+if (*string == 'a')
+{
+*string = '4';
+} else if (*string == 'e')
+{
+*string = '3';
+}
+else if (*string == 'g')
+{
+*string = '6';
+}
+else if (*string == 'i')
+{
+*string = '1';
+}
+else if (*string == 'o')
+{
+*string = '0';
+}
+else if (*string == 's')
+{
+*string = '5';
+}
+string++;
+}
+return string;
+}
