d2778ac57b5faec472b05fdd184d69dd6683b7ba
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-10-19 17:13:54 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - capitalizes all letter in s
+ * @string: a string
+ * Return: capped letters
+ */
+
+char *string_toupper(char *string)
+{
+while (*string != '\0')
+{
+if (*string >= 'a' && *string <= 'z')
+{
+*string = *string - 32;
+}
+string++;
+}
+}
