495e0be23afbf0b18f579cb0759cbb9a23c21d40
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-10-23 13:32:37 -0700

    First Commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_memset - fills the first n bytes of the memory area
+ *pointed to by s with the const byte b
+ *@s: string to be changed
+ *@b: constant byte
+ *@n: number of bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned char *p = (unsigned char *)s;
+
+while (n-- > 0)
+{
+*p++ = b;
+}
+return s;
+}
