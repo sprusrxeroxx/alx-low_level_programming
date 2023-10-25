463af1a86269edbbf646ea7a1abd31bb380c494a
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-10-25 13:18:25 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,8 +5,7 @@
  *_puts_recursion - prints a str follow by a '\n'
  *@s: a string
  */
-
-void _puts_recursion(char* s)
+void _puts_recursion(char *s)
 {
 if (*s != '\0')
 {
@@ -16,4 +15,3 @@ _puts_recursion(s + 1);
 if (*s == '\0')
 _putchar('\n');
 }
-
