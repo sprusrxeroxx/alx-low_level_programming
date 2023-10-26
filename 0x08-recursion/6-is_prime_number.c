4bb2e30cec235aa34bfd689772c62c5d674784b1
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-10-25 19:27:44 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,9 +3,9 @@
 /**
  *is_prime_number - determines if an int is prime or not
  *@n : input int
- *Returns: 1 or 0
+ *Return: 1 or 0
  */
-int is_prime_number(int n) 
+int is_prime_number(int n)
 {
 if (n < 2)
 {
@@ -15,7 +15,7 @@ if (n == 2 || n == 3)
 {
 return (1);
 }
-if (n % 2 == 0) 
+if (n % 2 == 0)
 {
 return (0);
 }
