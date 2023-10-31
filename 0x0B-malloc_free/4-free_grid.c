62a70e0d57171a7e985f8378a948163d0664146b
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-10-31 11:45:30 -0700

    4. It's not bragging if you can back it up

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,3 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *free_grid - frees grid created
+ *@grid: input
+ *@height: input
+ *Return: nothing
+ */
 void free_grid(int **grid, int height)
 {
 int i;
