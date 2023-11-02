c9d4e933e319940ccc83eb9e9a85cfd274f9a49a
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-11-01 17:11:23 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,34 +1,56 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 /**
- * main - prints out multiplication of 2 int
- * @argc: no of elements in argv
- * @argv: Vector arr
- * Return: 0 or 1
+ *verify_num - checks if str is dgt
+ *@str: a string
+ *Return: 0
+ */
+int verify_num(char *str)
+{
+	unsigned int count;
+
+	count =0;
+	while (count < strlen(str))
+	{
+		if (!(isdigit(str[count])))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+
+/**
+ *main - returns CLA addtion typed
+ *@argc: argument count
+ *@argv: argument vector
+ *Return: 0
  */
 int main(int argc, char *argv[])
- {
-if (argc < 2) 
 {
-return (0);
-printf("\n");
-}
+	int i, conv, res;
 
-int sum = 0;
+	i = 1;
+       	sum = 0;
+	while (i < argc)
+	{
+		if (verify_num(argv[count])
+		{
+		conv = atoi(argv[count]);
+		res += conv;
+		}
 
-for (int i = 1; i < argc; i++) 
-{
-int number = atoi(argv[i]);
-if (number > 0) 
-{
-sum += number;
-}
-else
-{
-return (1);
-break
-}
-}
-return (0);
+		else
+		{
+		printf("Error\n");
+		return (1);
+		}
+		i++;
+	}
+	printf("%d\n", res);
+	return (0);
 }
