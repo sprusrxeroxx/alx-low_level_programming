1958106891eba758ac6967a2171991f4057b4ff6
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-11-02 19:00:23 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	len = nmemb * size; 
+	len = nmemb * size;
 	ptr = malloc(len);
 
 	if (ptr == NULL)
