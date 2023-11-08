825b4c7a9acc9b72642c2344a8b0512a31fefd4e
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-11-07 17:23:57 -0800

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees all field in struct dog
+ * @d: field 
+ * Return: Nothing
+ */
+
+void free-dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
