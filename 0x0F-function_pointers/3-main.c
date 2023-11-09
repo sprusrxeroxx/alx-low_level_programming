a746892d9b081c985c0f8c6d2c8d92d89b6af955
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-11-08 17:36:58 -0800

    3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * main - prints results
+ * @argc: number of arguments 
+ * @argv: array of pointers 
+ * Return: 0
+ */
+
+int main(int _attribute_((_unused)) argc, char *argv[])
+{
+	int a, b;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	op = argv[2];
+	b = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (( *op == '/' && b == 0) || 
+	(*op == '%' && b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(a, b));
+	return (0);
+}
+
+
+
