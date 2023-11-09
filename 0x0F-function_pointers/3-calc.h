55d34fa9039c74e42496d339aa412dadff05dc83
ntokozovilakazi <ntokozovilakazi36@gmail.com>
2023-11-08 18:15:40 -0800

    3-main.c

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -11,7 +11,7 @@ typedef struct op
 {
 char *op;
 int (*f)(int a, int b);
-}op_t;
+} op_t;
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
@@ -20,4 +20,4 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
-#endif 
+#endif
