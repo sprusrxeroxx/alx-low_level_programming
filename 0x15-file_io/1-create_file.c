075a59a32047910560d7d6a3300aa4eff7677d7a
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2023-11-27 21:07:57 -0800

    Fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,28 +9,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file, cout = 0, res = 1;
+	int f, cout = 0, res = 1;
 
 	if (!filename)
 		return (-1);
 
-	file = open( O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	f = open(filename,O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (file == -1)
+	if (f == -1)
 		return (-1);
 
 	if (text_content)
 	{
 		while(text_content[cout])
 		cout++;
-		res = write(file, text_content, cout);
+		res = write(f, text_content, cout);
 	}
-	if (result == -1)
-		
+	if (res == -1)
+	{
 		return (-1);
-		close(file);
+	}
+		close(f);
 		return (1);
 }
-
-
-
