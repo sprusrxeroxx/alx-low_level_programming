d262cc1249c0024b9352d6262037c05660a9a7c2
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2023-11-27 20:43:22 -0800

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,95 @@
+#include "man.h"
+#define BUF_SIZE 1024
+
+/**
+ * main - main funct
+ * @argv: array argument pointer
+ * @argc: argument counter
+ * Return:0
+ */
+
+int main(int argc, char **argv)
+{
+	int f0, f1, ,res0, res 1;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "usage: cp file from file to \n");
+		exit(97);
+	}
+
+	buffer = malloc(sizeof(char)*BUF_SIZE);
+	if (!buffer)
+		return (0);
+
+	f1 = open(argv[1], O_RDONLY);
+	error_98(f1, buffer, argv[1]);
+	f0 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, ,0664);
+	error_99(f0, ,buffer, argv[2]);
+
+	do
+	{
+		res0 = read(f1, buffer, BUF_SIZE);
+		if (res0 == 0)
+			break;
+		error_98(res0, buffer, argv[1]);
+		res1 = write(f0, buffer, res0);
+		error_99(res1, buffer, argv[2]);
+	}
+	while (res1 > BUF_SIZE)
+		res0 = close(f1);
+		free(buffer);
+		return (0);
+}
+
+/**
+ * error_98 - checks for error
+ * @buffer: a buffer
+ * @argv: argument value
+ * @f0: value
+ */
+
+void error_98(int f0, char *buffer, char *argv)
+{
+	if (f0 < 0 )
+	{
+		dprintf(STDERR_FILEND, "Error; can't read from file %s\n", argv);
+		free(buffer);
+		exit(98);
+	}
+}
+
+/**
+ * error_99 - checks for error
+ * @buffer: a buffer
+ * @argv: argument value
+ * @f0: value
+ */
+
+void error_99(int f0, char *buffer, char *argv)
+{
+        if (f0 < 0 )
+        {
+                dprintf(STDERR_FILEND, "Error; can't write to %s\n", argv);
+                free(buffer);
+                exit(99);
+        }
+}
+
+/**
+ * error_100 - checks for error
+ * @buffer: a buffer
+ * @argv: argument value
+ * @f0: value
+ */
+
+void error_100(int f0, char *buffer, char *argv)
+{
+        if (f0 < 0 )
+        {
+                dprintf(STDERR_FILEND, "Error; can't close fd %i\n", f0);
+                free(buffer);
+                exit(100);
+        }
+}
