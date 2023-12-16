caa1e4804713de0fbc64b99def44aa3f77cc9c3c
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2023-12-16 10:19:59 -0800

    BETTY

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,16 +1,17 @@
 #include "main.h"
 #define BUF_SIZE 1024
 
+void error_99(int f0, char *buffer, char *argv);
+void error_98(int f0, char *buffer, char *argv);
+void error_100(int f0, char *buffer);
+
 /**
  * main - main funct
  * @argv: array argument pointer
  * @argc: argument counter
  * Return:0
+ *
  */
-
-void error_99(int f0, char *buffer, char *argv);
-void error_98(int f0, char *buffer, char *argv);
-void error_100(int f0, char *buffer);
 
 int main(int argc, char **argv)
 {
@@ -23,25 +24,24 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 
-	buffer = malloc(sizeof(char)*BUF_SIZE);
+	buffer = malloc(sizeof(char) * BUF_SIZE);
 	if (!buffer)
-		return (0);
+	return (0);
 
 	f1 = open(argv[1], O_RDONLY);
 	error_98(f1, buffer, argv[1]);
 	f0 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	error_99(f0, buffer, argv[2]);
 
-	do
-	{
+	do {
 		res0 = read(f1, buffer, BUF_SIZE);
 		if (res0 == 0)
-			break;
+		break;
+
 		error_98(res0, buffer, argv[1]);
 		res1 = write(f0, buffer, res0);
 		error_99(res1, buffer, argv[2]);
-	}
-	while (res0 > 0);
+	} while (res0 > 0);
 
 		res0 = close(f1);
 		free(buffer);
@@ -53,12 +53,13 @@ int main(int argc, char **argv)
  * @buffer: a buffer
  * @argv: argument value
  * @f0: value
+ *
  */
 
 void error_98(int f0, char *buffer, char *argv)
-
 {
-	if (f0 < 0) {
+	if (f0 < 0)
+	{
 	dprintf(STDERR_FILENO, "Error; can't read from file %s\n", argv);
 	free(buffer);
 	exit(98);
@@ -74,27 +75,27 @@ void error_98(int f0, char *buffer, char *argv)
 
 void error_99(int f0, char *buffer, char *argv)
 {
-        if (f0 < 0)
-        {
-                dprintf(STDERR_FILENO, "Error; can't write to %s\n", argv);
-                free(buffer);
-                exit(99);
-        }
+	if (f0 < 0)
+	{
+		dprintf(STDERR_FILENO, "Error; can't write to %s\n", argv);
+		free(buffer);
+		exit(99);
+	}
 }
 
 /**
  * error_100 - checks for error
  * @buffer: a buffer
- * @argv: argument value
  * @f0: value
+ *
  */
 
 void error_100(int f0, char *buffer)
 {
-        if (f0 < 0)
-        {
-                dprintf(STDERR_FILENO, "Error; can't close fd %i\n", f0);
-                free(buffer);
-                exit(100);
-        }
+	if (f0 < 0)
+	{
+		dprintf(STDERR_FILENO, "Error; can't close fd %i\n", f0);
+		free(buffer);
+		exit(100);
+	}
 }
