b81650934f43bd1f8973a6c1af6fb22187749421
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2023-12-16 05:31:40 -0800

    FIX 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - creates a file
  * @filename: a fileame
- * @text_content: a string 
+ * @text_content: a string
  * Return: 1 0r -1
  */
 
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fd = open(filename,O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	wr = write(fd, text_content, len);
 
